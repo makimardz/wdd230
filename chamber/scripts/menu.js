37a72143f209ebb019895f02a960e68ef3f35b06
makimardz <makimardz@gmail.com>
2023-01-21 22:34:53 +0800

    upddate

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,7 +1,6 @@
 const menubutton = document.querySelector("header button");
-const navi = document.querySelectorAll("nav");
-// menuitems will be a Node List of the list items
+const mynav = document.querySelector("nav");
 
 menubutton.addEventListener("click", () => {
-	navi.classList.toggle("open")
-});
+	mynav.classList.toggle("open")
+})
