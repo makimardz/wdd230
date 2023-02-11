9262d0b2083986bba203779e91b5fce238e88739
makimardz <makimardz@gmail.com>
2023-02-11 22:39:55 +0800

    update

--- scripts/rating.js
+++ scripts/rating.js
@@ -1,9 +1,75 @@
-const rating = document.getElementById("rating");
-const rangevalue = document.getElementById("r");
+document.getElementById("range").addEventListener("change", function(){
+    popup("the range was changed to value " + this.value);
+});
+document.getElementById("form").addEventListener("submit", function(e){
+    e.preventDefault();
 
-function displayRatingValue() {
-    rating.innerHTML = rangevalue.value;
-}
+    if(document.getElementById("user1").value != document.getElementById("user2").value){
+        popup("the usernames do not match");
+        return;
+    }
 
-rangevalue.addEventListener('change', displayRatingValue);
-rangevalue.addEventListener('input', displayRatingValue);
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var rating = document.getElementById("range").value;
+    var username = document.getElementById("user1").value;
+
+    var div = document.createElement("div");
+
+    var h2 = document.createElement("h2");
+    h2.innerHTML = "Form Input";
+    div.appendChild(h2);
+
+    var table = document.createElement("table");
+    var tr = document.createElement("tr");
+    var th = document.createElement("th");
+    th.innerHTML = "Field";
+    tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "Value";
+    tr.appendChild(th);
+    table.appendChild(tr);
+
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = "Name";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = name;
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = "Email";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = email;
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = "Rating";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = rating;
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    var tr = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = "Username";
+    tr.appendChild(td);
+    td = document.createElement("td");
+    td.innerHTML = username;
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    div.appendChild(table);
+
+    document.getElementById("formtable").appendChild(div);
+    document.getElementById("formtable").style.display = "block";
+
+    document.getElementById("form").reset();
+});
--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,18 +1,34 @@
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp')
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const API_KEY = 'cc62e66d8130004846514727c1635ade';
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
-const apiKey = 'cc62e66d8130004846514727c1635ade';
-const city = 'Trier';
-const units = 'imperial';
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}';
-
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => displayData(data));
-
-    function displayData(data) {
-        console.log(data)
-
-    }
+// Get the current location of the user
+navigator.geolocation.getCurrentPosition(async position => {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  
+  // Make a request to the OpenWeather API to retrieve the weather information
+  const response = await fetch(`${API_URL}&lat=${lat}&lon=${lon}`);
+  const data = await response.json();
+  
+  // Extract the relevant information from the API response
+  const temp = data.main.temp;
+  const icon = data.weather[0].icon;
+  const description = data.weather[0].description;
+  
+  // Create the HTML elements to display the information
+  const weatherIcon = document.createElement('img');
+  weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  weatherIcon.alt = description;
+  
+  const tempDisplay = document.createElement('p');
+  tempDisplay.textContent = `Temperature: ${temp}°C`;
+  
+  const caption = document.createElement('figcaption');
+  caption.appendChild(weatherIcon);
+  caption.appendChild(tempDisplay);
+  
+  // Add the HTML elements to the page
+  const figure = document.createElement('figure');
+  figure.appendChild(caption);
+  document.body.appendChild(figure);
+});
