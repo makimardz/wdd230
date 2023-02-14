029afe405a771f6f80c3e386e8d425c722f13490
makimardz <makimardz@gmail.com>
2023-02-14 12:12:33 +0800

    update

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -7,40 +7,43 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 //member informaion
+
 fetch("members.json")
   .then(response => response.json())
   .then(data => {
-    let members = data;
-    Object.values(members).forEach(function(member) {
-        let card = document.createElement("div");
-        card.classList.add("card");
-      
-        let h2 = document.createElement("h2");
-        h2.textContent = member.name;
-        card.appendChild(h2);
-      
-        let p1 = document.createElement("p");
-        p1.textContent = member.address;
-        card.appendChild(p1);
-      
-        let p2 = document.createElement("p");
-        p2.textContent = member.phone;
-        card.appendChild(p2);
-      
-        let a = document.createElement("a");
-        a.textContent = member.website;
-        a.href = member.website;
-        card.appendChild(a);
-      
-        let img = document.createElement("img");
-        img.src = member.icon;
-        img.alt = `${member.name} icon`;
-        card.appendChild(img);
-      
-        let p3 = document.createElement("p");
-        p3.textContent = `Membership Level: ${member.level}`;
-        card.appendChild(p3);
-      
-        document.querySelector("#cards").appendChild(card);
-      });
+    let members = data.members;
+    let cardsContainer = document.querySelector(".cards-container");
+
+    members.forEach(function(member) {
+      let card = document.createElement("div");
+      card.classList.add("card");
+
+      let h2 = document.createElement("h2");
+      h2.textContent = member.name;
+      card.appendChild(h2);
+
+      let p1 = document.createElement("p");
+      p1.textContent = member.address;
+      card.appendChild(p1);
+
+      let p2 = document.createElement("p");
+      p2.textContent = member.phone;
+      card.appendChild(p2);
+
+      let a = document.createElement("a");
+      a.textContent = member.website;
+      a.href = member.website;
+      card.appendChild(a);
+
+      let img = document.createElement("img");
+      img.src = member.icon;
+      img.alt = `${member.name} icon`;
+      card.appendChild(img);
+
+      let p3 = document.createElement("p");
+      p3.textContent = `Membership Level: ${member.membershipLevel}`;
+      card.appendChild(p3);
+
+      cardsContainer.appendChild(card);
   });
+});
